219e73208e03a228c50012d0ca6410a2362bd763
ayusu89 <ayusapaudel_25@eemc.edu.np>
2026-05-27 05:35:15 +0545

    Circular Enqueue

---
+++ circular queue/enqueue.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#define max 5
+int queue [max];
+int data;
+int front = -1;
+int rear = -1;
+void enqueue (int data)
+{
+    if((rear + 1)%max == front)
+    {
+        printf("Queue is full \n");
+    }
+    else if (front == -1 && rear == -1)
+    {
+        front = rear = 0;
+        queue[rear]=data;
+        printf("Enqueued element : %d \n",data);
+    }
+    else 
+    {
+        queue[rear]=data;
+        printf("Enqueued element = %d \n",data);
+        rear ++ ;
+    }
+}
+int main()
+{
+    enqueue(10);
+    enqueue(20);
+    enqueue(30);
+    enqueue(40);
+    enqueue(50);
+    enqueue(60);
+    return 0;
+}
