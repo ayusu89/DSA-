51834b464eede4c4f92e90f18d1f8f995a6b44c4
ayusu89 <ayusapaudel_25@eemc.edu.np>
2026-06-04 05:22:25 +0545

    LINKED LIST

---
+++ LinkedList/insert-at-beginning.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+struct node *insert_at_beginning(struct node *head)
+{
+    struct node *newnode;
+    int i;
+
+    newnode = (struct node *) malloc(sizeof(struct node));
+
+    if (newnode == NULL)
+    {
+        printf("Memory not allocated\n");
+        return head;
+    }
+     
+     printf("Enter Data: ");
+     scanf("%d", &newnode->data);
+
+     newnode->next = head;
+     head = newnode;
+
+     
+   
+    return head;
+}
+
+int main()
+{
+    struct node *head = NULL;
+    
+
+    
+       head = insert_at_beginning(head);
+       printf("Inserted value = %d\n", head->data);
+
+
+    
+
+   
+    return 0;
+}
