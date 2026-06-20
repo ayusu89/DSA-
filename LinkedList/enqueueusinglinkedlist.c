1e1fd1daee606d229ba92562166bfb6276f42dea
ayusu89 <ayusapaudel_25@eemc.edu.np>
2026-06-20 15:43:06 +0545

    LINKEDLISTQUEUE

---
+++ LinkedList/enqueueusinglinkedlist.c
@@ -0,0 +1,76 @@
+// enqueue using linked list
+#include<stdio.h>
+#include<stdlib.h>
+struct node 
+{
+    struct node *next ;
+    int data;
+};
+struct node *newnode ;
+struct node *front = NULL;
+struct node *rear = NULL;
+
+// function to insert element into the queue 
+
+void enqueue (int data )
+{
+    newnode = (struct node *)malloc(sizeof (struct node ));
+    if(newnode==NULL)
+    {
+        printf("MEMORY NOT ALLOCATED \n");
+        return ;
+    }
+    newnode -> data = data;
+    newnode->next = NULL;
+    
+    if (rear == NULL)
+    {
+        front = rear = newnode ;
+    }
+    
+    else
+
+    {
+        rear -> next = newnode ;
+        rear = newnode ;
+    }
+
+    printf("Enqueued = %d \n",data);
+}
+
+// function to dequeue the element from the queue 
+
+void dequeue ()
+{
+    if (front == NULL)
+     {
+        printf("Queue is empty.\n");
+        return ;      // indicate failure
+     }
+    struct node* temp = front;
+    int data = temp->data;
+    front = front->next;
+
+    if (front == NULL)
+     {
+        rear = NULL;  // important fix
+     }
+
+    free(temp);
+    printf("Dequeued: %d\n", data);
+    return  ;
+}
+
+// program execution
+
+int main()
+{
+    enqueue(10);
+    enqueue(20);
+    enqueue(30);
+    dequeue();
+    dequeue();
+    dequeue();
+    return 0 ;
+}
+
