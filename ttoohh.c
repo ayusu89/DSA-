907791da0affce61be57ff8cb841c4b6b0e59440
ayusu89 <ayusapaudel_25@eemc.edu.np>
2026-07-24 10:15:05 +0545

    final

---
+++ ttoohh.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void toh (int n , char source , char dest , char aux )
+{
+    if(n==1)
+    {
+        printf("Move 1 disk from %c to %c \n", source , dest);
+        return ;
+    }
+    toh (n-1 , source , aux , dest);
+    printf("Move %d from %c to %c \n" ,n, source , dest);
+
+    toh(n-1 , aux , dest , source );
+}
+int main ()
+{
+    int n ;
+    printf("Enter the number of disks : \n");
+    scanf("%d",&n);
+    toh(n,'A','C','B');
+    return 0;
+}
