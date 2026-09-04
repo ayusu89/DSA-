e0931bf2e9b4008aae046b29a3424943f5ed5c81
ayusu89 <ayusapaudel_25@eemc.edu.np>
2026-09-04 11:39:21 +0545

    ALXI LAGO DOUBLE

---
+++ doublylinkedlist/deletefrombeg.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+    struct node *prev;
+};
+
+// Function to delete a node from the beginning
+
+struct node *deletefrombeginning(struct node *head)
+{
+    struct node *temp;
+
+    // Check if the list is empty
+    if (head == NULL)
+    {
+        printf("The list is empty . Nothing to delete.\n");
+        return head;
+    }
+
+    // Point temp to the first node
+    temp = head;
+
+    // Move head to the second node
+    head = head->next;
+
+    // If there is a second node,
+    // remove its backward link
+    if (head != NULL)
+    {
+        head->prev = NULL;
+    }
+
+    // Free the old first node
+    free(temp);
+
+    printf("Node deleted from the beginning.\n");
+
+    return head;
+}
+
+int main()
+{
+    struct node *head = NULL;
+    struct node *newnode;
+    struct node *temp;
+    // Delete from beginning
+
+    head = deletefrombeginning(head);
+
+    return 0;
+}
